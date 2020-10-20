2ef0f223488b22642d89c011a4c8cb215b3aee57
Daniel Soto <dasg.1993@gmail.comm>
2020-10-19 20:00:12 -0500

    [feature] - array method examples

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Semana 3 -Sesión 01
+// Every
+const isAdult = (people) => {
+    return people.age >= 18
+}
+
+const data = [
+    {
+        name: 'Daniel',
+        age: 27
+    },
+    {
+        name: 'Jose',
+        age: 17
+    },
+    {
+        name: 'Pedro',
+        age: 20
+    }
+];
+
+console.log(data.every(isAdult))
+
+// Filter
+console.log(data.filter(isAdult))
+// Find
+console.log(data.find(isAdult))
+// Map
+console.log(data.map(item => {
+    item.age += 1
+    return item;
+}))
